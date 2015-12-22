60106fd9f6344a3d4f22d139e66c36b25688e3ec
Bryce <brycekbargar@gmail.com>
2015-12-21 21:11:33 -0600

    RGR Make a VM class

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const VM = require('./../index.js');
+
+describe('For the ChucK VM', () => {
+  beforeEach('Setup VM', () => {
+    this.chuck = new VM();
+  });
+  it('expect it to be ok!', () => {
+  });
+});
